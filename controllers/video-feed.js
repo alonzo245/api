927ca4a09873e094479c6b12bb7ce02915387be4
Alon Alush <alon.a@syte-vc.com>
2018-12-29 19:54:41 +0200

    added iredis

--- controllers/video-feed.js
+++ controllers/video-feed.js
@@ -5,11 +5,8 @@ var slash = require('slash');
 
 // const redis = require('redis');
 // const client = redis.createClient();
-var redis = require('redis');
-var url = require('url');
-var redisURL = url.parse(process.env.REDISCLOUD_URL);
-var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-client.auth(redisURL.auth.split(":")[1]);
+var Redis = require('ioredis');
+var redis = new Redis(process.env.REDIS_URL);
 client.on('error', (err) => {
   console.log("Error " + err);
 });
